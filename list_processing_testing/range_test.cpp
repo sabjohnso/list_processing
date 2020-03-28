a121a7a9a03f9d00a774ea466b7f39409d0475cf
Samuel B. Johnson <sabjonso@yahoo.com>
2020-03-28 11:20:03 -0500

    removed obsolete files

---
+++ list_processing_testing/range_test.cpp
@@ -0,0 +1,12 @@
+//
+// ... Testing header files
+//
+#include <gtest/gtest.h>
+
+namespace ListProcessing::Testing
+{
+
+  TEST(Range, Construction1)
+  {}
+
+} // end of namespace ListProcessing::Testing
